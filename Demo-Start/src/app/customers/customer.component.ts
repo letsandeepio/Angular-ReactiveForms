a97d218d7d9b1366389231956c94479fcabe2b5b
Sandeep Chopra <sandeepchopra7@gmail.com>
2023-05-01 13:48:49 -0400

    simplify the error messages using error object keys

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -43,7 +43,12 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
-  emailMessage = 'Please ensure correct email address';
+  emailMessage: string;
+
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.',
+  };
 
   constructor(private fb: FormBuilder) {}
 
@@ -69,6 +74,11 @@ export class CustomerComponent implements OnInit {
     this.customerForm
       .get('notification')
       .valueChanges.subscribe((value) => this.setNotification(value));
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.subscribe((value) =>
+      this.setMessage(emailControl)
+    );
   }
 
   save(): void {
@@ -95,5 +105,14 @@ export class CustomerComponent implements OnInit {
     }
 
     phoneControl.updateValueAndValidity();
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors)
+        .map((key) => this.validationMessages[key])
+        .join(' ');
+    }
   }
 }
